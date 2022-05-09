4beacc4dc2e9418646c83a1a7cda76fa0d8065f7
Samer-Smati <samersmati@gmail.om>
2022-05-09 17:38:22 +0200

    dom checkpoint

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let btnHeart = document.getElementsByClassName('fa-heart');
+let btnDelete = document.getElementsByClassName('fa-trash');
+let BtnPlus = document.getElementsByClassName('fa-plus-circle')
+let BtnMinus = document.getElementsByClassName('fa-minus-circle')
+
+for (let index = 0; index < btnHeart.length; index++) {
+    btnHeart[index].addEventListener('click',function (){
+        console.log(this.style.color);
+        if(this.style.color == 'red'){
+            this.style.color = ''
+        }else{
+            this.style.color = 'red';
+        }
+    })
+}
+
+for (let index = 0; index < btnDelete.length; index++) {
+    btnDelete[index].addEventListener('click',function (){
+        this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
+    })
+}
